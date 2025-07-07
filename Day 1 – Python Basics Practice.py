40374156da355851a4dc13d0f3052110a158d77d
DineshSelvam14 <dinesh.selvam1412@gmail.com>
2025-07-07 10:32:25 +0530

    Add files via upload
    Day 1 – Python Variables, Data Types, Input/Output Practice

---
+++ Day 1 – Python Basics Practice.py
@@ -0,0 +1,7 @@
+name = (input("enter your name: "))
+age = int(input("enter your age: "))
+height = float(input("enter your height:"))
+
+print(f"Hello  {name} ")
+print(f"you are {age} years old")
+print(f"your height is : {height}")
