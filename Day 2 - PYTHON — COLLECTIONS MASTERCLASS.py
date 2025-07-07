e12089b7c32dcdcb4a0a939d10d82aa092495b23
DineshSelvam14 <dinesh.selvam1412@gmail.com>
2025-07-07 18:28:52 +0530

    Day 2 - PYTHON — COLLECTIONS MASTERCLASS
    List
    Tuples
    set
    dictionary
    type

---
+++ Day 2 - PYTHON — COLLECTIONS MASTERCLASS.py
@@ -0,0 +1,30 @@
+fruits = ["orange", "Apple", "banana"]
+print(fruits[0])
+fruits.append("mango")
+print(fruits)
+fruits.remove ("banana")
+print(fruits)
+print(fruits[2])
+color = ("red", "orange", "white", "green")
+print(color)
+print(color[2])
+num = {1,2,3,3,5}
+print(num)
+num.add(4)
+print(num)
+person = {"name" : "dani", "age" : 32, "height" : 5.8}
+print(person)
+person["city"] = "Salem"
+print(person)
+favfood = ["Briyani", "Sambar rice", "dosa" , "curd rice", "chapathi"]
+print(f"my fav foods are:", favfood[0], "and", favfood[4]) 
+favcolors=("Orange", "white", "Blue", "Green")
+print(f"my fav colors are:",favcolors)
+SetNums = {1,2,3,4,4,3,2,1,0,5,6,9,7,9,6,5,4,8,8,8}
+print(SetNums)
+mydictinoary = {"name" : "Dinesh", "age" : 32, "city" : "Salem", "Favsport" : "Cricket"}
+print(f"my personal dictinoary:", mydictinoary)
+print("List type:" , type(favfood))
+print("List type:" , type(favcolors))
+print("List type:" , type(SetNums))
+print("List type:" , type(mydictinoary))
